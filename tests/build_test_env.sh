a1efaba5d748c160c7f595552d40802929bba1c7
kainwen <kainwen@gmail.com>
2017-03-26 21:58:45 +0800

    fix test

--- tests/build_test_env.sh
+++ tests/build_test_env.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
-compile_c_code () {
-    mkdir build; cd build;
-    cmake -DCMAKE_INSTALL_PREFIX=../local ..;
-    make;
-	make install;
-    cd ..;
-}
-
 install_python () {
-	pip install git+https://github.com/kainwen/lambdaOJ2@master\#egg=lambdaOJ2\&subdirectory=python_package
+    cd python_package;
+    python setup.py install;
+    rm build -rf;
+    cd ..
 }
 
-compile_c_code;
 install_python;
--- tests/run.sh
+++ tests/run.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-export JUDGE_DIR=`pwd`/local/bin;
-export LD_LIBRARY_PATH=`pwd`/local/lib;
 python tests/test.py;
